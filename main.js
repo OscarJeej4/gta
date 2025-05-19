99d7b3497a299a26818e2f229cd2f96bb2cf5b68
OscarJeej4 <oscarjeej@outlook.com>
2025-05-19 11:17:27 +0100

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,9 @@ function init() {
   scene = new THREE.Scene();
   scene.background = new THREE.Color(0xa0a0a0);
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 5, 10);
+  camera.position.set(0, 50, 50);
+  camera.lookAt(0, 0, 0);
+
 
   // Renderer
   renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('game') });
